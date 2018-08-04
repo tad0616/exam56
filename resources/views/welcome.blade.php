97fb4e9ed5dd6ad0526f528753935d71bd22428c
Tad <tad0616@gmail.com>
2018-08-04 12:25:19 +0800

    瞭解路由的運作

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{$name}} {{$say}} Laravel
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,12 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'tad';
+    $say  = '嗨！';
+    return view('welcome', compact('name', 'say'));
 });
 
 Auth::routes();
