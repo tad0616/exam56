7b88d830b6d116ead8e6756c6fb01dd9f325ab3b
Tad <tad0616@gmail.com>
2018-08-04 15:22:31 +0800

    修改路由改用控制器

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -13,7 +13,7 @@ class ExamController extends Controller
      */
     public function index()
     {
-        //
+        return view('welcome');
     }
 
     /**
@@ -23,7 +23,7 @@ class ExamController extends Controller
      */
     public function create()
     {
-        //
+        return view('exam.create');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/exam/create', function () {
-    return view('exam.create');
-})->name('exam.create');
+Route::get('/exam', 'ExamController@index')->name('exam.index');
+Route::get('/exam/create', 'ExamController@create')->name('exam.create');
