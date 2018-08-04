c760a65aa6767ba597aa4e7bfc1eb84bdf9bcb83
Tad <tad0616@gmail.com>
2018-08-04 17:58:12 +0800

    網站的關閉與啟動

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,60 +1,14 @@
-<html>
-  <head>
-    <title>{{ config('backpack.base.project_name') }} Error 503</title>
+@extends('layouts.app')
+@section('content')
+  @component('bs::jumbotron', ['fluid' => true])
+      @slot('heading')
+          503 網站目前進廠維護中
+      @endslot
+      @slot('subheading')
+          沒什麼大事啦～例行維護嘛...您懂的...
+      @endslot
 
-    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
-    <style>
-      body {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-        color: #B0BEC5;
-        display: table;
-        font-weight: 100;
-        font-family: 'Lato';
-      }
-
-      .container {
-        text-align: center;
-        display: table-cell;
-        vertical-align: middle;
-      }
-
-      .content {
-        text-align: center;
-        display: inline-block;
-      }
-
-      .title {
-        font-size: 156px;
-      }
-
-      .quote {
-        font-size: 36px;
-      }
-
-      .explanation {
-        font-size: 24px;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <div class="content">
-        <div class="title">503</div>
-        <div class="quote">It's not you, it's me.</div>
-        <div class="explanation">
-          <br>
-          <small>
-            <?php
-              $default_error_message = "The server is overloaded or down for maintenance. Please try again later.";
-            ?>
-            {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
-         </small>
-       </div>
-      </div>
-    </div>
-  </body>
-</html>
+      <hr class="my-3">
+      <p>不就是改改程式，抓抓臭蟲...應該不用兩三天就好了啦！</p>
+  @endcomponent
+@endsection
