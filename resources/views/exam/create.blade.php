e71cc46f7d5734c45709fc8600d5d165c8afd191
Tad <tad0616@gmail.com>
2018-08-04 15:18:09 +0800

    建立發佈測驗的表單

--- resources/views/exam/create.blade.php
+++ resources/views/exam/create.blade.php
@@ -1,4 +1,30 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 @section('content')
     <h1>{{ __('Create Exam') }}</h1>
+    @can('建立測驗')
+        {{ bs()->openForm('post', '/exam') }}
+            {{ bs()->formGroup()
+                    ->label('測驗標題', false, 'text-sm-right')
+                    ->control(bs()->text('title')->placeholder('請填入測驗標題'))
+                    ->showAsRow() }}
+
+            {{ bs()->formGroup()
+                    ->label('測驗狀態', false, 'text-sm-right')
+                    ->control(bs()->radioGroup('enable', [1 => '啟用', 0 => '關閉'])
+                                ->selectedOption(1)
+                                ->inline())
+                    ->showAsRow() }}
+
+            {{ bs()->formGroup()
+                    ->label('')
+                    ->control(bs()->submit('建立測驗'))
+                    ->showAsRow() }}
+        {{ bs()->closeForm() }}
+    @else
+        @component('bs::alert', ['type' => 'danger'])
+            @slot('heading')
+            無建立測驗的權限
+            @endslot
+        @endcomponent
+    @endcan
 @endsection
