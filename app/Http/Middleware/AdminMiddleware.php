e53212ca3c4969bc1f1a9587914569663ef064bd
Tad <tad0616@gmail.com>
2018-08-04 14:45:59 +0800

    讓閒雜人等不能進入後台

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->user()->hasAnyPermission('後台管理')) {
+            abort(403);
+        }
+        return $next($request);
+    }
+}
