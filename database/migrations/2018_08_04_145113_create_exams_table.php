5a0212512804e19eb54d38dd682b45c0e94cd281
Tad <tad0616@gmail.com>
2018-08-04 14:56:09 +0800

    編輯並執行測驗（exam）的 migrate 檔案

--- database/migrations/2018_08_04_145113_create_exams_table.php
+++ database/migrations/2018_08_04_145113_create_exams_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateExamsTable extends Migration
 {
@@ -15,6 +15,10 @@ class CreateExamsTable extends Migration
     {
         Schema::create('exams', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->boolean('enable');
             $table->timestamps();
         });
     }
